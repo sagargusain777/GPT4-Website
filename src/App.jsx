225f07d1e723805c3c3a67b41dc932fea2558ae7
uwuxverticuse <sagargusain150@gmail.com>
2024-02-25 23:21:28 +0530

    Done some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App (){
+import {Header,Footer,Blog,Features,WhatGPT4, Possibility,} from "./containers/Containers.jsx";
+import { CTA, Brand, Navbar } from "./components/Components.jsx";
+import "./App.css";
 
-   return (
-        <div>
-
-        </div>
-   )
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT4 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 export default App;
--- src/Components/Components.jsx
+++ src/Components/Components.jsx
@@ -1,6 +1,6 @@
- export { default as Article} from './Components/article';
- export { default as Brand} from './Components/brand';
- export { default as CTA} from './Components/cta';
- export { default as feature} from './Components/feature';
- export { default as Navbar} from './Components/navbar';
+ export { default as Article} from './article/Article';
+ export { default as Brand} from './brand/Brand';
+ export { default as CTA} from './cta/CTA';
+ export { default as feature} from './feature/Feature';
+ export { default as Navbar} from './navbar/Navbar';
  
