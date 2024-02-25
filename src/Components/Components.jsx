83e290dd9abf4a23b92180ffb72144afc18f0338
uwuxverticuse <sagargusain150@gmail.com>
2024-02-25 17:56:27 +0530

    Structured  Component Folder

---
+++ src/Components/Components.jsx
@@ -0,0 +1,6 @@
+ export { default as Article} from './Components/Article';
+ export { default as Blog} from './Components/Blog';
+ export { default as CTA} from './Components/CTA';
+ export { default as feature} from './Components/feature';
+ export { default as Navbar} from './Components/navbar';
+ 
---
+++ src/Components/cta/CTA.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CTA.css";
+const CTA = () => {
+  
+    return (
+    <div>CTA</div>
+    )
+};
+export default CTA;
